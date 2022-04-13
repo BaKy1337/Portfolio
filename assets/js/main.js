8ea8ba17eb60941aab9fc859a58c6dea035f3ce8
BaKy1337 <a.abhilil2@gmail.com>
2022-04-13 13:54:37 +0200

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,18 @@ themeButton.addEventListener('click', () => {
 
 
 /* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, {delay: 500})
+sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__social`, {delay: 700})
+sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
+sr.reveal(`.profile__buttons`, {delay: 800})
+sr.reveal(`.profile__content`, {delay: 900})
+sr.reveal(`.filters`, {delay: 1000})
